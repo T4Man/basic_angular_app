d8ff5a88ab8c39d8484ddbee8ef9e8261b1d880c
t4man <timothywforman@gmail.com>
2016-05-11 09:45:54 -0700

    add linter and edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const eslint = require('gulp-eslint');
+const files = ['**/*.js', '!build/**', '!node_modules/**'];
 
 gulp.task('webpack:dev', () => {
   gulp.src('app/js/entry.js')
     .pipe(webpack( {
+      devtool: 'source-map',
       output: {
         filename: 'bundle.js'
       }
@@ -16,5 +19,12 @@ gulp.task('static:dev', () => {
     .pipe(gulp.dest('./build'));
 });
 
+gulp.task('lint:nontest', () => {
+  return gulp.src(files)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);
 gulp.task('default', ['build:dev']);
+gulp.task('lint', ['lint:nontest']);
