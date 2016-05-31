5991ef7b20ecb02fe640bafa45fb606ea51263d3
t4man <timothywforman@gmail.com>
2016-05-31 08:58:49 -0700

    edit gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const eslint = require('gulp-eslint');
+const protractor = require('gulp-protractor').protractor;
+const cp = require('child_process');
+
+var children = [];
 const files = ['**/*.js', '!build/**', '!node_modules/**', '!test/**'];
 
 gulp.task('webpack:dev', () => {
@@ -12,6 +16,35 @@ gulp.task('webpack:dev', () => {
       }
     }))
     .pipe(gulp.dest('./build'));
+});
+
+gulp.task('webpack:protractor', () => {
+  gulp.src('test/e2e/**.js')
+    .pipe(webpack( {
+      devtool: 'source-map',
+      output: {
+        filename: 'pro_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest( './test'));
+});
+
+gulp.task('protractor:test', ['build:dev', 'startservers:test'], () => {
+  gulp.src('test/e2e/*spec.js')
+    .pipe(protractor( {
+      configFile: 'test/e2e/config.js'
+    }))
+    .on('end', () => {
+      children.forEach((child) => {
+        child.kill('SIGTERM');
+      });
+    });
+});
+
+gulp.task('startservers:test', (done) => {
+  children.push(cp.fork('server.js'));
+  children.push(cp.spawn('webdriver-manager', ['start']));
+  setTimeout(done, 1000);
 });
 
 gulp.task('static:dev', () => {
@@ -31,6 +64,7 @@ gulp.task('lint:server', () => {
     .pipe(eslint.format());
 });
 
+gulp.task('test', ['protractor:test', 'webpack:protractor']);
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);
-gulp.task('default', ['build:dev', 'lint']);
 gulp.task('lint', ['lint:browser', 'lint:server']);
+gulp.task('default', ['build:dev', 'lint', 'test']);
